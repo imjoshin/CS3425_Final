bbeea4db4f4a72de4ad8ad1fdbaa66e306a3126c
j0sh77 <jocjohns@mtu.edu>
2016-11-27 16:11:38 -0600

    Added login from another project

--- scripts/functions.php
+++ scripts/functions.php
@@ -9,4 +9,22 @@ function func(){
 
 }
 
+
+/*
+FUTURE USE:
+
+function login($username, $pass){
+  $username = preg_replace('/[^\w]/', '', $username);
+  $pass = preg_replace('/[^\w]/', '', $pass);
+  $stmt = db_op("select address from users where username = '$username' and pass = '$pass'");
+  if($stmt->num_rows != 1){
+    return json_encode(array("error"=>"Invalid username or password."));
+  }
+  $row = $stmt->fetch_array(MYSQLI_ASSOC);
+  $_SESSION["address"] = $row["address"];
+  $_SESSION["user"] = $username;
+  return json_encode(array());
+}
+*/
+
 ?>
